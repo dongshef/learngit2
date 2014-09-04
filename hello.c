2a062b267848290f9657cbc75107e25a5c7ad3d6
dongshef <dongsheng.fan@alcatel-lucent.com>
2014-09-04 16:42:46 +0800

    add new function

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
    printf("hello world\n");
    return 0;
-}   
+}
+
+void add(int a, int b)
+{
+    printf("hello\n");
+}
